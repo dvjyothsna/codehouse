04982b088e59bb69d9a1a97e78c08f487307442f
Michelle Ahn <michelleahn@berkeley.edu>
2017-07-20 23:18:47 -0700

    changed home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,19 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component } from "@angular/core";
+ import { Component, OnInit } from "@angular/core";
+ import { Socket } from 'ng-socket-io';
+
+ import { Observable } from 'rxjs/Observable';
+ import { Subscription } from 'rxjs/Subscription';
+ import 'rxjs/add/operator/map';
 
 @Component({
     styleUrls: ['./home.component.scss'],
     templateUrl: './home.component.html',
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit{
     CODELIST: string[] = [];
     selected: number = 0;
     COLOURLIST: string[] = [];
@@ -19,6 +24,12 @@ export class HomeComponent {
     LINE_THREE: string[] = [];
     LINE_FOUR: string[] = [];
 
+    pointsSub: Subscription;
+    opponentScore = 0;
+    constructor(private socket:Socket) {}
+    ngOnInit() {
+    this.subscribeToNotifications();
+        }
 
     clickLabel(labelNumber: number) {
         this.selected = labelNumber;
@@ -28,5 +39,23 @@ export class HomeComponent {
     }
     run(lineNumber: number) {
 
+    }
+
+
+    sendPoints() {
+        let msg = "Hey";
+        this.socket.emit("message" , 10);
+    }
+    getPoints() {
+        let msg = "Hii";
+        return this.socket
+            .fromEvent("messageReturn")
+            .map(data => data);
+    }
+    subscribeToNotifications() {
+      this.pointsSub = this.getPoints().subscribe(
+        (data) => {
+          this.opponentScore = this.opponentScore + data["points"];
+        });
     }
 }
