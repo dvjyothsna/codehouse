a0cb38e77a8106957f05a180b4b70210f579dc81
Michelle Ahn <michelleahn@berkeley.edu>
2017-07-21 01:17:01 -0700

    all of michelles changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,13 +16,13 @@
 })
 
 export class HomeComponent implements OnInit{
-    CODELIST: string[] = [];
-    selected: number = 0;
-    COLOURLIST: string[] = [];
-    LINE_ONE: string[] = ["MAX", "=", "APPLE #1"];
-    LINE_TWO: string[] = [];
-    LINE_THREE: string[] = [];
-    LINE_FOUR: string[] = [];
+    correct_lines = 0;
+    CODELIST: string[][] = [[" "," "," ", " "],[" "," "," "," "],[" "," "," "," "],[" "," "," "," "],[" "," "," "," ", " "]];
+    selected_line: number = 4;
+    selected_word: number = 4;
+    COLOURLIST: string[][] = [["primary","primary","primary", "primary"],["primary","primary","primary","primary"],["primary","primary","primary","primary"],["primary","primary","primary", "primary"],["primary","primary","primary", "primary", " "]];
+    SOLUTION: string[][] = [["MAX", "=", "APPLE #1", " "], ["FOR EACH", "APPLE", "IN", "BASKET"],
+    ["IF", "APPLE", ">", "MAX"], ["MAX", "=", "APPLE", " "], ["BUY", "MAX", " ", " "]];
 
     pointsSub: Subscription;
     opponentScore = 0;
@@ -31,14 +31,53 @@ export class HomeComponent implements OnInit{
     this.subscribeToNotifications();
         }
 
-    clickLabel(labelNumber: number) {
-        this.selected = labelNumber;
+    clickLabel(lineNumber: number, wordNumber: number) {
+        this.selected_line = lineNumber;
+        this.selected_word = wordNumber;
     }
+
     clickButton(buttonText: string) {
-        this.CODELIST[this.selected] = buttonText;
+        this.CODELIST[this.selected_line][this.selected_word] = buttonText;
+        this.COLOURLIST[this.selected_line][this.selected_word] = "primary";
     }
     run(lineNumber: number) {
-
+        if(this.CODELIST[lineNumber][0] == this.SOLUTION[lineNumber][0] &&
+        this.CODELIST[lineNumber][1] == this.SOLUTION[lineNumber][1] &&
+        this.CODELIST[lineNumber][2] == this.SOLUTION[lineNumber][2] &&
+        this.CODELIST[lineNumber][3] == this.SOLUTION[lineNumber][3]
+        ) {
+            this.COLOURLIST[lineNumber][0] = "danger\" disabled";
+            this.COLOURLIST[lineNumber][1] = "danger\" disabled";
+            this.COLOURLIST[lineNumber][2] = "danger\" disabled";
+            this.COLOURLIST[lineNumber][3] = "danger\" disabled";
+            this.selected_line = 4;
+            this.selected_word = 4;
+            this.correct_lines += 1;
+            if(this.correct_lines == 5) {
+                alert("You have completed this level!");
+            }
+        } else {
+            if(this.CODELIST[lineNumber][0] == this.SOLUTION[lineNumber][0]) {
+                this.COLOURLIST[lineNumber][0] = "success";
+            } else {
+                this.COLOURLIST[lineNumber][0] = "warning";
+            }
+            if(this.CODELIST[lineNumber][1] == this.SOLUTION[lineNumber][1]) {
+                this.COLOURLIST[lineNumber][1] = "success";
+            } else {
+                this.COLOURLIST[lineNumber][1] = "warning";
+            }
+            if(this.CODELIST[lineNumber][2] == this.SOLUTION[lineNumber][2]) {
+                this.COLOURLIST[lineNumber][2] = "success";
+            } else {
+                this.COLOURLIST[lineNumber][2] = "warning";
+            }
+            if(this.CODELIST[lineNumber][3] == this.SOLUTION[lineNumber][3]) {
+                this.COLOURLIST[lineNumber][3] = "success";
+            } else {
+                this.COLOURLIST[lineNumber][3] = "warning";
+            }
+        }
     }
 
 
